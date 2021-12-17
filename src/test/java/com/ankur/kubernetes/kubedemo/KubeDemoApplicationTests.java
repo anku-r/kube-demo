3197f0034bc2feec533ce3cdbbe4bc7086ed0b7f
Ankur Sharma <anksharm39@publicisgroupe.net>
2021-12-17 12:56:27 +0530

    Initial commit

---
+++ src/test/java/com/ankur/kubernetes/kubedemo/KubeDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ankur.kubernetes.kubedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KubeDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
