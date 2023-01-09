7456ccd76090bb26ebd9f4c27cfd8d2891d448c9
Ankur Sharma <anksharm39@publicisgroupe.net>
2023-01-09 13:30:01 +0530

    Nothing

--- src/main/java/com/ankur/kubernetes/kubedemo/HomeController.java
+++ src/main/java/com/ankur/kubernetes/kubedemo/HomeController.java
@@ -1,13 +1,33 @@
 package com.ankur.kubernetes.kubedemo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.time.Instant;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 @RestController
 public class HomeController {
 
-    @RequestMapping("/")
-    public String home() {
-        return "Running";
+    private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+
+    @CrossOrigin(origins = "http://localhost:3000")
+    @RequestMapping("/home")
+    public Map<String, Object> home() throws UnknownHostException {
+        InetAddress server = InetAddress.getLocalHost();
+        logger.info("Accessing Application Home");
+        Map<String, Object> data = new LinkedHashMap<>();
+        data.put("status", "Running");
+        data.put("datetime", Instant.now());
+        data.put("host", server.getHostName());
+        data.put("address", server.getHostAddress());
+        return data;
     }
 }
